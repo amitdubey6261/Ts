11b4834937b7f99f6536ffeffe8a6305a9b7867b
amit dubey <amitdubey6261@gmail.com>
2023-05-12 00:43:23 +0530

    variables-2

---
+++ Variables/Variables.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nam = "kv1Chakeri";
+nam = "ooao"; // allowed
+// nam = 10 // not allowed and decvlared by underline
+// nam.toUpperCase() //not allowed in case of numbers
+console.log(nam);
---
+++ Variables/Variables.ts
@@ -0,0 +1,22 @@
+let nam:string = "kv1Chakeri" ;
+
+nam = "ooao" // allowed
+
+// nam = 10 // not allowed and decvlared by underline
+// nam.toUpperCase() //not allowed in case of numbers
+
+console.log(nam)
+
+// numbers in ts
+
+// let val: number = 10000 ------------> bad practise  , 
+
+let val = 1000 // ----------> in this case type inference is done which is defining that in a val we are using is a number automitacall
+
+console.log(val)
+
+//boolean 
+
+let x: boolean  = true
+
+export {} //temporary stops the errors
