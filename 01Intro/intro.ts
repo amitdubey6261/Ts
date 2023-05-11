f9e4848fc701c6f67a912f321747a314d5487d1a
amit dubey <amitdubey6261@gmail.com>
2023-05-12 00:23:42 +0530

    step-1

---
+++ 01Intro/intro.ts
@@ -0,0 +1,5 @@
+let obj = {name : "amit" , class : "10" , enrl : 10}
+
+console.log(obj.enrl)
+console.log("hello amit ")
+console.log(obj.sports) //-----------> line indicating that property not exists
